8f9953a1c850e975fb6f3f8fbae952d877af759f
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-10-08 13:36:22 +0200

    The project was refactored to follow clean object-oriented design:  each class has clear responsibility, input is validated, data is protected, and the code now uses proper Java and JUnit 5 best practices.

--- src/main/java/org/ejercicio2/IdCalculation.java
+++ src/main/java/org/ejercicio2/IdCalculation.java
@@ -2,11 +2,16 @@ package org.ejercicio2;
 
 public class IdCalculation {
     // Array with letters corresponding to DNI numbers
-    private static final char[] Letters = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+    private static final char[] LETTERS = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
 
     // Method that calculates the DNI letter based on the number
-    public static char calculateLetter (int numberId){
-        int index = numberId % 23; // remainder after dividing by 23
-        return Letters[index];    // return the corresponding letter
+    public static char calculateLetter(int numberId) {
+        if (numberId < 0 && numberId > 99999999) {
+            throw new IllegalArgumentException("The number of ID cannot be negative or superior to 8 digit");
+        }
+            int index = numberId % 23; // remainder after dividing by 23
+            return LETTERS[index];    // return the corresponding letter
+        }
     }
-}
+
+
